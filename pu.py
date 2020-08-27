41c758e2491d25eab5d5eab56267269206f0e39c
Aaron Stanek <amr.stanek@gmail.com>
2020-08-27 14:43:48 -0700

    removed unused determine_valid_chars

--- pu.py
+++ pu.py
@@ -205,58 +205,6 @@ def load_command_line_parameters():
     random_seed = str(sys.argv).encode("UTF-8")
     return valid_chars, length, random_seed
 
-def determine_valid_chars(char_sets):
-    # translate shorthand codes
-    if char_sets == "a":
-        char_sets = "clnps"
-    elif char_sets == "ar":
-        char_sets = "clnrs"
-    elif char_sets == "z":
-        char_sets = "clnp"
-    elif char_sets == "zr":
-        char_sets = "clnr"
-    # build valid_chars set
-    valid_chars = set()
-    for set_name in char_sets:
-        if set_name == "c" or set_name == "u":
-            # capital letters
-            chars = list(range(65,91))
-        elif set_name == "l":
-            # lowercase letters
-            chars = list(range(97,123))
-        elif set_name == "n":
-            # numbers
-            chars = list(range(48,58))
-        elif set_name == "p":
-            # punctuation
-            chars = [
-                "`","~","!","@","#",
-                "$","%","^","&","*",
-                "(",")","-","_","=",
-                "+","[","{","]","}",
-                "\\","|",";",":","\'",
-                "\"",",","<",".",">",
-                "/","?"
-                ]
-            chars = list(map(ord,chars))
-        elif set_name == "r":
-            # restricted punctuation
-            chars = [
-                "!","@","#","$","%",
-                "&","*","(",")","-",
-                "_","+","[","{","]",
-                "}",";",":",",",".",
-                "?"
-                ]
-            chars = list(map(ord,chars))
-        elif set_name == "s":
-            chars = [32]
-        else:
-            raise ValueError("unknown character set: "+set_name)
-        for x in chars:
-            valid_chars.add(x)
-    return valid_chars
-
 def main():
     valid_chars, length, random_seed = load_command_line_parameters()
     password = generate_password(length, random_seed, valid_chars)
