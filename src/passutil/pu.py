c6354fa8062746613cfc710e914040918e85bf31
Aaron Stanek <amr.stanek@gmail.com>
2021-08-06 14:05:30 -0700

    can no longer call pu.py directly as a script, feature became impossible to use as of 2.0.0 and was replaced in 2.0.1 by calling __main__.py

--- src/passutil/pu.py
+++ src/passutil/pu.py
@@ -112,30 +112,3 @@ def generate_password(length,key,valid_chars):
     # convert to a string
     password = bytes(password).decode("UTF-8")
     return password
-
-def load_command_line_parameters():
-    if len(sys.argv) == 2:
-        if (sys.argv[1] == "help"):
-            raise Exception("Full documentation at: https://github.com/aaronstanek/PythonPasswordUtility")
-        if (sys.argv[1] == "hash"):
-            raise Exception("Using SHA-"+str(SHA512_number)+" 512")
-    if len(sys.argv) < 3:
-        raise ValueError("not enough command line parameters")
-    valid_chars = sys.argv[1]
-    try:
-        length = int(sys.argv[2])
-    except:
-        raise TypeError("second command line argument should be an integer")
-    key = str(sys.argv).encode("UTF-8")
-    return valid_chars, length, key
-
-def main():
-    try:
-        valid_chars, length, key = load_command_line_parameters()
-        password = generate_password(length, key, valid_chars)
-        print(password)
-    except Exception as ex:
-        print(ex)
-
-if __name__ == "__main__":
-    main()
