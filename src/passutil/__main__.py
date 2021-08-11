d7d147599799acb14c80053cfc47a2dc80772cf4
Aaron Stanek <amr.stanek@gmail.com>
2021-08-10 19:33:49 -0700

    rewrote load_command_line_parameters

--- src/passutil/__main__.py
+++ src/passutil/__main__.py
@@ -10,23 +10,23 @@ def load_command_line_parameters():
         # user did not give any input
         # display a welcome message
         raise Exception("Python Password Utility\nCryptographically secure, easy-to-use, password generator\nFull documentation at: https://github.com/aaronstanek/PythonPasswordUtility")
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "--help":
-            # user entered --help
-            # direct them to documentation
-            raise Exception("Full documentation at: https://github.com/aaronstanek/PythonPasswordUtility")
-        if sys.argv[1] == "--hash":
-            # user entered --hash
-            # tell them which version of SHA512 we are using
-            raise Exception("Using SHA-"+str(SHA512_number)+" 512")
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == "--size":
-            # user entered --size
-            # we know that there is another parameter after --size
-            # treat it like a charstring
-            # then give the size of the resulting charset
-            raise Exception(str(charset_size(sys.argv[2])))
-    if len(sys.argv) < 3:
+    elif sys.argv[1] == "--help":
+        # user entered --help
+        # direct them to documentation
+        raise Exception("Full documentation at: https://github.com/aaronstanek/PythonPasswordUtility")
+    elif sys.argv[1] == "--hash":
+        # user entered --hash
+        # tell them which version of SHA512 we are using
+        raise Exception("Using SHA-"+str(SHA512_number)+" 512")
+    elif sys.argv[1] == "--size":
+        # user entered --size
+        # check if there is another parameter after --size
+        if len(sys.argv) < 3:
+            raise Exception("expected valid_chars parameter after --size")
+        # treat it like a charstring
+        # then give the size of the resulting charset
+        raise Exception(str(charset_size(sys.argv[2])))
+    elif len(sys.argv) < 3:
         raise ValueError("not enough command line parameters")
     # there is at least charset and length
     valid_chars = sys.argv[1]
@@ -34,7 +34,7 @@ def load_command_line_parameters():
         # we expect the length to be a valid integer
         length = int(sys.argv[2])
     except:
-        raise TypeError("second command line argument should be an integer")
+        raise TypeError("length parameter should be an integer")
     key = str(sys.argv)
     return valid_chars, length, key
 
